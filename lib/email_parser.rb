842b2f7a4bbb190b07989d6bfd77fe0894228982
Carrie Savinell <carrielane89@gmail.com>
2018-05-22 18:23:46 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -17,7 +17,7 @@ class EmailParser
   #separate into unique email address 
   def parse
     emails.split.collect do |address| 
-      address.split(",")
+      address.split(",").join
     end.flatten.uniq!
   end
 end   
